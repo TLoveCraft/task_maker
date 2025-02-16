e260e55695c3d34d2d538e088349f560e3d1eabf
ThierryA <thiaramend04@gmail.com>
2025-02-16 16:22:17 -0300

    UPDATE: USER VIEW HTML

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Task_Maker</title>
 </head>
 <body>
+    <h3>Bem vindo!</h3>
 
 </body>
 </html>

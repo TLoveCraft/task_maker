aeb0b929cd490c0b742605d0caf672a44bfd40ff
ThierryA <thiaramend04@gmail.com>
2025-02-16 16:42:28 -0300

    CREATE: USER CREATE FUNCTION

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,4 +9,8 @@ class UserController extends Controller
     public function index() {
         return view('users.index');
     }
+    public function create() {
+        //Isso carrega a view create.blade.php dentro da pasta resources/views/users
+        return view('users.create');
+    }
 }
